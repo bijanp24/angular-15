eafb6f05373054072ae47aeedec81699f7662a36
bijanp24 <57022377+bijanp24@users.noreply.github.com>
2023-04-20 11:20:37 -0700

    mat grid working with mat form fields

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import 'zone.js/dist/zone';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+//import { MatAutocompleteModule } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'my-app',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatGridListModule, MatFormFieldModule],
   templateUrl: 'blah.html',
 })
 export class App {
